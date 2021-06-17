b8e17e9bb32b5e4b735745a3bf91320cb5d60851
bidhan <yamk1029@gmail.com>
2021-06-17 08:18:38 +0545

    bulb onoff

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 function changeMyColor(color)
-{
+/*{
     var myp = document.getElementById('merodesh');
     myp.style.backgroundColor = color;
+}*/
+
+function onoff(btn)
+{
+    var bulb = document.getElementById('bulb');
+    var status = bulb.getAttribute('src');
+
+    if(status == 'img/off.png')
+    {
+        bulb.setAttribute('src','img/on.png');
+        btn.innerText = "off";
+    }
+    else
+    {
+        bulb.setAttribute('src','img/off.png');
+        btn.innerText = "on"; 
+    }
 }
