eecd47370d931a6d0917ee0e39bd6bdd55928bca
bidhan <yamk1029@gmail.com>
2021-06-17 21:24:12 +0545

    first commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,8 @@
-$('#btnhide').click(function(){
-    $('#merodesh').hide();
+$('#btn').click(function (){
+  $('#mero').fadeIn(5000);
 });
-
-
-$('#btnshow').click(function(){
-    $('#merodesh').show();
-});
-
-
-
-$('#btn').click(function(){
-    $('#merodesh').toggle();
+$('#btn2').click(function (){
+  $('#mero').fadeOut(5000);
 });
 
 
@@ -54,25 +46,90 @@ $('#btn').click(function(){
 
 
 
-function changeMyColor(color)
-{
-    var myp = document.getElementById('merodesh');
-    myp.style.backgroundColor = color;
-}
 
-function onoff(btn)
-{
-    var bulb = document.getElementById('bulb');
-    var status = bulb.getAttribute('src');
 
-    if(status == 'img/off.png')
-    {
-        bulb.setAttribute('src','img/on.png');
-        btn.innerText = "off";
-    }
-    else
-    {
-        bulb.setAttribute('src','img/off.png');
-        btn.innerText = "on"; 
-    }
-}
+
+
+// $('#btnhide').click(function(){
+//     $('#merodesh').hide();
+// });
+
+
+// $('#btnshow').click(function(){
+//     $('#merodesh').show();
+// });
+
+
+
+// $('#btn').click(function(){
+//     $('#merodesh').toggle();
+// });
+
+
+//   });
+
+
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function changeMyColor(color)
+// {
+//     var myp = document.getElementById('merodesh');
+//     myp.style.backgroundColor = color;
+// }
+
+// function onoff(btn)
+// {
+//     var bulb = document.getElementById('bulb');
+//     var status = bulb.getAttribute('src');
+
+//     if(status == 'img/off.png')
+//     {
+//         bulb.setAttribute('src','img/on.png');
+//         btn.innerText = "off";
+//     }
+//     else
+//     {
+//         bulb.setAttribute('src','img/off.png');
+//         btn.innerText = "on"; 
+//     }
+// }
